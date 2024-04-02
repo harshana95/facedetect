476aaa85f7ea27b58b3ecba601e76d6fc3fbaa2a
Harshana <wweligam@purdue.edu>
2024-04-02 19:32:59 -0400

    crop image

--- create_data.sh
+++ create_data.sh
@@ -2,11 +2,11 @@
 module load use.own
 module load conda-env/mypackages-py3.8.5
 
-rm ./test_faces/*
+rm /scratch/gilbreth/wweligam/FaceData/test_faces/*
 python preprocess_data.py --batch_size 64  --dataset_path "/scratch/gilbreth/wweligam/FaceData/test"
 
-rm ./train_small_faces/*
+rm /scratch/gilbreth/wweligam/FaceData/train_small_faces/*
 python preprocess_data.py --batch_size 64  --dataset_path "/scratch/gilbreth/wweligam/FaceData/train_small"
 
-rm ./train_faces/*
+rm /scratch/gilbreth/wweligam/FaceData/train_faces/*
 python preprocess_data.py --batch_size 64  --dataset_path "/scratch/gilbreth/wweligam/FaceData/train"
